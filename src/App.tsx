ecbc5499cde5aca90a3635100b4bdb7f228ba4c9
rolud <rolud.dev@gmail.com>
2022-12-09 16:25:49 +0100

    Initialize project using Create React App

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>miauu</h1>
+      <h3>a teachers' community</h3>
+    </div>
+  );
+}
+
+export default App;
